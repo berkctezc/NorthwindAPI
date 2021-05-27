e6b3d8587814fcc2a2063adf3cba9436210ae9a6
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-27 21:17:02 +0300

    added context and efproductdall

---
+++ DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -0,0 +1,15 @@
+﻿using Entitites.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+   public  class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
