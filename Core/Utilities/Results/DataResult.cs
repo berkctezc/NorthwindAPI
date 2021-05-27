2bb934f31523e81d1cb076e58a3c4ac2d42fa921
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-27 22:11:37 +0300

    added results

---
+++ Core/Utilities/Results/DataResult.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.Utilities.Results
+{
+    public class DataResult<T> : Result, IDataResult<T>
+    {
+        public DataResult(T data, bool success, string message) : base(success, message)
+        {
+            Data = Data;
+        }
+
+        public DataResult(T data, bool success) : base(success)
+        {
+            Data = Data;
+        }
+
+        public T Data { get; }
+    }
+}
