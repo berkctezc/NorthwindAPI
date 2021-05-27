4ce8c4dd71cd10ea42617c9650ee476a7c8003fe
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-27 19:39:47 +0300

    created ientityrepository

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using Core.Entities;
@@ -9,5 +10,10 @@ namespace Core.DataAccess
 {
     public interface IEntityRepository<T> where T:class,IEntity
     {
+        T Get(Expression<Func<T,bool>> filter);
+        IList<T> GetList(Expression<Func<T,bool>> filter=null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
     }
 }
--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.DataAccess;
+using Entitites.Concrete;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal
-    {
-    }
+    public interface IProductDal : IEntityRepository<Product> { }
 }
