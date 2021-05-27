9c1e806a54e08060495f534485ad57e5da4e8870
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-27 22:11:57 +0300

    implemented product

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entitites.Concrete;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        Product GetById(int productId);
+        List<Product> GetList();
+        List<Product> GetListByCategory(int categoryId);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
